e22ea5cba63e7eae424b97d97c323a5eec945702
feranzie <oladaviesferanmi@gmail.com>
2023-04-21 07:04:38 -0700

    model logic update

--- model.py
+++ model.py
@@ -52,3 +52,67 @@ class ModelService:
                 if i not in current_user['category'].unique():
                     recomendation.append(i)
         return recomendation[:10]
+    
+    def lambda_handler(self, event):
+        # print(json.dumps(event))
+
+        predictions_events = []
+
+        for record in event["Records"]:
+            encoded_data = record["kinesis"]["data"]
+            ride_event = base64_decode(encoded_data)
+
+            # print(ride_event)
+            ride = ride_event["ride"]
+            ride_id = ride_event["ride_id"]
+
+            features = self.prepare_features(ride)
+            prediction = self.predict(features)
+
+            prediction_event = {
+                "model": "ride_duration_prediction_model",
+                "version": self.model_version,
+                "prediction": {"ride_duration": prediction, "ride_id": ride_id},
+            }
+
+            for callback in self.callbacks:
+                callback(prediction_event)
+
+            predictions_events.append(prediction_event)
+
+        return {"predictions": predictions_events}
+
+class KinesisCallback:
+    def __init__(self, kinesis_client, prediction_stream_name):
+        self.kinesis_client = kinesis_client
+        self.prediction_stream_name = prediction_stream_name
+
+    def put_record(self, prediction_event):
+        ride_id = prediction_event["prediction"]["ride_id"]
+
+        self.kinesis_client.put_record(
+            StreamName=self.prediction_stream_name,
+            Data=json.dumps(prediction_event),
+            PartitionKey=str(ride_id),
+        )
+def create_kinesis_client():
+    endpoint_url = os.getenv("KINESIS_ENDPOINT_URL")
+
+    if endpoint_url is None:
+        return boto3.client("kinesis")
+
+    return boto3.client("kinesis", endpoint_url=endpoint_url)
+
+def init(prediction_stream_name: str, run_id: str, test_run: bool):
+    model = load_model(run_id)
+
+    callbacks = []
+
+    if not test_run:
+        kinesis_client = create_kinesis_client()
+        kinesis_callback = KinesisCallback(kinesis_client, prediction_stream_name)
+        callbacks.append(kinesis_callback.put_record)
+
+    model_service = ModelService(model=model, model_version=run_id, callbacks=callbacks)
+
+    return model_service
