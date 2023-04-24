8b389838ad2f329572ee871658e8bb757b32a9ef
feranzie <oladaviesferanmi@gmail.com>
2023-04-24 08:15:58 -0700

    event stream infrastructure added

---
+++ infrastructure/main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_version = ">= 1.0"
+  backend "s3" {
+    bucket  = "feranmi-state-mlops-zoomcamp"
+    key     = "mlops-zoomcamp-stg.tfstate"
+    region  = "eu-west-1"
+    encrypt = true
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+#instanciate login
+data "aws_caller_identity" "current_identity" {}
+
+locals {
+  account_id = data.aws_caller_identity.current_identity.account_id
+}
+
+# recommender_events
+module "source_kinesis_stream" {
+  source = "./modules/kinesis"
+  retention_period = 48
+  shard_count = 2
+  stream_name = "${var.source_stream_name}-${var.project_id}"
+  tags = var.project_id
+}
---
+++ infrastructure/variables.tf
@@ -0,0 +1,13 @@
+variable "aws_region" {
+  description = "AWS region to create resources"
+  default     = "eu-west-1"
+}
+
+variable "project_id" {
+  description = "project_id"
+  default = "recommender-system"
+}
+
+variable "source_stream_name" {
+  description = ""
+}
