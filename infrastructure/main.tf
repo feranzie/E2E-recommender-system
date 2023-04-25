1ea9692a0d47f4e9fc89ec7b2749713d22e6a9cc
feranzie <oladaviesferanmi@gmail.com>
2023-04-25 11:51:54 -0700

    lambda module added

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -50,3 +50,14 @@ module "ecr_image" {
    lambda_function_local_path = var.lambda_function_local_path
    docker_image_local_path = var.docker_image_local_path
 }
+
+module "lambda_function" {
+  source = "./modules/lambda"
+  image_uri = module.ecr_image.image_uri
+  lambda_function_name = "${var.lambda_function_name}_${var.project_id}"
+  model_bucket = module.s3_bucket.name
+  output_stream_name = "${var.output_stream_name}-${var.project_id}"
+  output_stream_arn = module.output_kinesis_stream.stream_arn
+  source_stream_name = "${var.source_stream_name}-${var.project_id}"
+  source_stream_arn = module.source_kinesis_stream.stream_arn
+}
